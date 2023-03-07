f6fb6b2158356bd39857e38bdec9e1e5d64cbc97
rajethanm4 <rajethanm4@gmail.com>
2023-03-07 11:10:12 +0530

    second commit

---
+++ ft.rb
@@ -0,0 +1,26 @@
+# even and odd no
+a = gets.chomp.to_i
+if a%2==0
+  puts "even no"
+else
+  puts "odd no"
+end 
+
+number=[]
+
+number << 1
+number << 2
+number << 3
+
+for items in number
+  if items%2==0
+    puts "even no"
+  else puts "nothing"
+  end
+end
+
+
+
+
+
+
